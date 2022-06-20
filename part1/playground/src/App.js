3992f00e9b5d08664f494b16b1b694a86fac7eae
Utrehd <samuel.a@marti.to>
2022-06-21 01:18:49 +0200

    Inital Commit Unicafe

--- part1/playground/src/App.js
+++ part1/playground/src/App.js
@@ -1,19 +1,60 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
-      {right}
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const LabledButton = (props) => {
+  return (
+    <div>
+      <h1>{props.label}</h1>
+      <Button onClick={props.onClick} text={props.text}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const incrementLeft = () => {
+    setLeft(left + 1)
+    addClick('L')
+  }
+  const [right, setRight] = useState(0)
+  const incrementRight = () => {
+    setRight(right + 1)
+    addClick('R')
+  }
+
+  const [allClicks, setAll] = useState([])
+  const addClick = (click) => setAll(allClicks.concat(click))
+
+  return (
+    <div>
+      <History allClicks={allClicks}/>
+      <LabledButton onClick={incrementLeft} text="Increment Left" label={right}/>
+      <LabledButton onClick={incrementRight} text="Increment Right" label={left}/>
+    </div>
+  )
+}
+
+export default App
