ea9980262703cf96c0650a5c3b002182798c5bac
Utrehd <samuel.a@marti.to>
2022-06-21 22:26:47 +0200

    1.13*: anecdotes step2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -14,15 +14,26 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-   
+  
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState([0,0,0,0,0,0,0]) 
+
   const random = () => Math.floor(Math.random() * anecdotes.length)
   const randomAnecdote = () => setSelected(random)
+
+  const voteAnecdote = () => {
+    const newVotes = { ...votes}
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
   console.log('random number: ', selected)
   return (
     <React.StrictMode>
       <div>
         <H2Text text={anecdotes[selected]}/>
+        <H2Text text={'has ' + votes[selected] + ' votes'}/>
+        <Button text={'vote for anecdote'} handleClick={voteAnecdote}/>
         <Button text={'next anecdote'} handleClick={() => randomAnecdote()}/>
       </div>
     </React.StrictMode>
