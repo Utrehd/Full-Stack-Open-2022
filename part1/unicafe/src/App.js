305f8eda572137a369dd3db52292b43d497c30c9
Utrehd <samuel.a@marti.to>
2022-06-21 02:09:28 +0200

    Finish of Part 1 Step 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 
+const H1Text = ({text}) => <h1>{text}</h1>
+const LabeldValue = ({text, value}) => <p>{text} : {value}</p>
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const incrementValue = (parameter, update) => update(parameter + 1)
+ //<Button handleClick={incrementValue(good, setGood)} text={'Vote for Good'}/>
   return (
     <div>
-      code here
+      <H1Text text={'Give Feedback'}/>
+      <Button handleClick={() => incrementValue(good, setGood)} text={'Vote for Good'}/>
+      <Button handleClick={() => incrementValue(neutral, setNeutral)} text={'Vote for Good'}/>
+      <Button handleClick={() => incrementValue(bad, setBad)} text={'Vote for Good'}/>
+      <H1Text text={'Statistics'}/>
+      <LabeldValue text={'Good'} value={good}/>
+      <LabeldValue text={'Neutral'} value={neutral}/>
+      <LabeldValue text={'Bad'} value={bad}/>
     </div>
   )
 }
