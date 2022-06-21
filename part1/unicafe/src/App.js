c465f17864f1d8b1a3a236ef8d48b3055f8adbf2
Utrehd <samuel.a@marti.to>
2022-06-21 02:55:11 +0200

    1.9: unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 
 const H1Text = ({text}) => <h1>{text}</h1>
+const H2Text = ({text}) => <h2>{text}</h2>
 const LabeldValue = ({text, value}) => <p>{text} : {value}</p>
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
-const Statistics = ({good,neutral,bad}) => {
-  const total = good + neutral + bad
+
+const Statistics = ({good,bad,total}) => {  
   return (
     <div>
       <LabeldValue text={'Total'} value={total}/>
@@ -13,6 +14,7 @@ const Statistics = ({good,neutral,bad}) => {
     </div>
   )
 }
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -20,19 +22,34 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const incrementValue = (parameter, update) => update(parameter + 1)
- //<Button handleClick={incrementValue(good, setGood)} text={'Vote for Good'}/>
-  return (
+
+  const header = (
     <div>
       <H1Text text={'Give Feedback'}/>
       <Button handleClick={() => incrementValue(good, setGood)} text={'Vote for Good'}/>
       <Button handleClick={() => incrementValue(neutral, setNeutral)} text={'Vote for Neutral'}/>
       <Button handleClick={() => incrementValue(bad, setBad)} text={'Vote for Bad'}/>
       <H1Text text={'Statistics'}/>
+    </div>
+  )
+
+  const total = good + neutral + bad
+  if(total === 0){
+    return (
+      <div>
+        {header}
+        <H2Text text={'No Feedback Given'}/>
+      </div>
+    )
+  } 
+ 
+ return (
+    <div>
+      {header}
       <LabeldValue text={'Good'} value={good}/>
       <LabeldValue text={'Neutral'} value={neutral}/>
       <LabeldValue text={'Bad'} value={bad}/>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
-
+      <Statistics good={good} bad={bad} total={total}/>
     </div>
   )
 }
