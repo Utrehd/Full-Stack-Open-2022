5b221d8921e3f466c695a9e0cda687f5a918b35d
Utrehd <samuel.a@marti.to>
2022-07-05 17:26:35 +0200

    fix: 2.13*: Data for countries, step2

--- part2/countries/src/components/CountryDisplay.js
+++ part2/countries/src/components/CountryDisplay.js
@@ -1,15 +1,9 @@
-import {useState} from 'react'
 import React from 'react'
 import Text from './Text'
-import List from './List'
 import CountryList from './CountryList'
 import CountrySpecification from './CountrySpecification'
 
 const CountryDisplay = ({countries, handlerButton}) => {
-
-  //const [selectedCountry, setSelectedCountry] = useState('')
-
-
   let len = (countries.length)
   if (len === 1){
     console.log('country', countries[0])
@@ -18,7 +12,6 @@ const CountryDisplay = ({countries, handlerButton}) => {
 
   if (len <= 10){
     return(<CountryList items={countries} path={['name', 'common']} handlerButton={handlerButton}/>)
-    //return(<CountryList items={countries} path={['name', 'common']}/>)
   }
 
   if (len > 10){
